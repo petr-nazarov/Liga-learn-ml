d7c9b5edadf04916d0922bcf93fd14cdd8aee7c2
Petr Nazarov <mrzueck@gmail.com>
2023-08-26 08:43:42 +0000

    working text and colors

--- sports_dataloader.py
+++ sports_dataloader.py
@@ -1,8 +1,20 @@
 import os 
 import numpy as np 
 import cv2
-from typing import List, Dict
+import random
+from typing import List, Dict, Tuple
 
+
+def create_random_color(existing_colors : List[Tuple[int]]) -> Tuple[int, int, int]: 
+    # color in rgb format
+    new_color = (
+            int(np.random.randint(0, 256)),
+            int(np.random.randint(0, 256)),
+            int(np.random.randint(0, 256)),
+        )
+    if new_color in existing_colors: 
+        return create_random_color(existing_colors)
+    return new_color
 def load_gt(dataset_path: str): 
     gt_path = None
     gt_data= None
@@ -38,6 +50,9 @@ def load_img_paths(dataset_path: str) -> Dict[int, str]:
 def run_dataset(dataset_path:str , outdir: str): 
     img_paths = load_img_paths(dataset_path)
     gt_objects = load_gt(dataset_path)
+    colots_list = []
+    for _ in range(12):
+        colots_list.append(create_random_color(colots_list))
     for frame_number, frame_objects in gt_objects.items():
         try: 
             img_path = img_paths[frame_number]
@@ -46,14 +61,29 @@ def run_dataset(dataset_path:str , outdir: str):
         img = cv2.imread(img_path)
         img2draw = img.copy() 
         for player_id, player_bbox in frame_objects.items():
+            color = colots_list[player_id]
             cv_bbox = xywh2x1y1x2y2(player_bbox)
+            cv2.putText(
+                img2draw, 
+                text = str(player_id), 
+                # bottom left corner of the text
+                org = (
+                    #x1 + half of width of bbox
+                    cv_bbox[0] + int( (cv_bbox[2] - cv_bbox[-0]) /2) - 5, 
+                    # y1 - 2px
+                    cv_bbox[1]-5), 
+                fontFace = cv2.FONT_HERSHEY_SIMPLEX, 
+                fontScale = 1, 
+                color = color, 
+                thickness = 2, 
+                lineType = cv2.LINE_AA)
             cv2.rectangle(
                     img2draw, 
                     #top left 
                     pt1=(cv_bbox[0], cv_bbox[1]),
                     #bottom rigth
                     pt2=(cv_bbox[2], cv_bbox[3]),
-                    color=(0,0,0),
+                    color=color,
                     thickness=2)
 
         os.makedirs(outdir, exist_ok=True)
