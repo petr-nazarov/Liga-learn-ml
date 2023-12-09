8adf9ae4c0dffa9081dec27392d310185e03b803
Petr Nazarov <mrzueck@gmail.com>
2023-12-09 10:22:28 +0000

    fix pip3

--- image_convolution2.py
+++ image_convolution2.py
@@ -3,7 +3,7 @@ import numpy as np
 import albumentations as A
 import cv2
 from varname.helpers import debug
-from sports_dataloader_count_finder import load_img_paths
+from sports_dataloader import load_img_paths
 
 
 
